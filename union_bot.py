d92bf452ab574053e1d5ee2bd3ca01cfa86e4049
kenny <fedoranisimov@gmail.com>
2023-05-01 10:11:34 +0300

    button works normal

--- union_bot.py
+++ union_bot.py
@@ -24,6 +24,29 @@ menu_keyboard = ReplyKeyboardMarkup(
     keyboard=[["Главная", "Афиша"], ["Исполнители", "Билеты"]]
 )
 
+MAX_MESSAGE_LENGTH = 900
+
+def split_text(text):
+    if len(text) < MAX_MESSAGE_LENGTH:
+        return [text]
+
+    # Разделяем текст на подстроки по пробелам
+    substrings = text.split(' ')
+
+    result = []
+    current_substring = ''
+
+    for substring in substrings:
+        if len(current_substring) + len(substring) < MAX_MESSAGE_LENGTH:
+            current_substring += ' ' + substring
+        else:
+            result.append(current_substring.strip())
+            current_substring = substring
+
+    result.append(current_substring.strip())
+
+    return result
+
 # Определение обработчиков команд. Обычно они принимают два аргумента: update и context.
 async def start(update, context):
     """Отправка сообщения, когда пользователь вводит команду /start."""
@@ -79,7 +102,7 @@ async def afisha_callback(update, context):
     # Получение выбранного события
     selected_event = [event for event in context.user_data["afisha_card"] if event[2][0] == query.data.split("_")[1]][0]
     # Отправка фото
-    await context.bot.send_photo(chat_id=query.message.chat_id, photo=selected_event[4])
+    #await context.bot.send_photo(chat_id=query.message.chat_id, photo=selected_event[4])
     # Создание кнопок
     buy_button = InlineKeyboardButton(text="Купить билет", url=selected_event[5])
     more_info_button = InlineKeyboardButton(text="Подробнее", callback_data=f"more_info_{selected_event[2][0]}")
@@ -89,9 +112,11 @@ async def afisha_callback(update, context):
     keyboard = [[buy_button, more_info_button], [prev_button, next_button]]
     markup = InlineKeyboardMarkup(inline_keyboard=keyboard)
     # Создание текста сообщения
-    text = f"{selected_event[0]}\n\n{selected_event[2][1]} - {selected_event[3][1]}"
+    text = f"{selected_event[0]}\n\n{selected_event[2][1]} - {selected_event[3][1]} - {selected_event[2][0]}"
     # Отправка сообщения
-    await context.bot.send_message(chat_id=query.message.chat_id, text=text, reply_markup=markup)
+    #await context.bot.send_message(chat_id=query.message.chat_id, text=text, reply_markup=markup)
+    await context.bot.send_photo(chat_id=query.message.chat_id, photo=selected_event[4], caption=text,
+                                 reply_markup=markup)
     #media = InputMediaPhoto(media=selected_event[4], caption=text)
     #await query.edit_message_media(media=media, reply_markup=markup)
 
@@ -103,17 +128,49 @@ async def more_info_callback(update, context):
     await query.answer()
     # Получение выбранного события
     selected_event = [event for event in context.user_data["afisha_card"] if event[2][0] == query.data.split("_")[2]][0]
+    txt = split_text(selected_event[1])
+
     # Создание кнопок
     buy_button = InlineKeyboardButton(text="Купить билет", url=selected_event[5])
     prev_button = InlineKeyboardButton(text="<", callback_data=f"prev_{selected_event[2][0]}")
     next_button = InlineKeyboardButton(text=">", callback_data=f"next_{selected_event[2][0]}")
-    # Создание разметки с кнопками
+    if len(txt) > 1:
+        read_more = InlineKeyboardButton(text="дальше", callback_data=f"read_more_{selected_event[2][0]}")
+        keyboard = [[buy_button, read_more], [prev_button, next_button]]
+        markup = InlineKeyboardMarkup(inline_keyboard=keyboard)
+        # Редактирование сообщения
+        text = f"{selected_event[0]}\n\n{selected_event[2][1]} - {selected_event[3][1]} - {selected_event[2][0]}\n\n{txt[0]}"
+    else:
+        # Создание разметки с кнопками
+        keyboard = [[buy_button], [prev_button, next_button]]
+        markup = InlineKeyboardMarkup(inline_keyboard=keyboard)
+        # Редактирование сообщения
+        text = f"{selected_event[0]}\n\n{selected_event[2][1]} - {selected_event[3][1]} - {selected_event[2][0]}\n\n{selected_event[1]}"
+    #await query.edit_message_text(text=text, reply_markup=markup)
+    await context.bot.edit_message_media(chat_id=query.message.chat_id, message_id=query.message.message_id,
+                                         media=InputMediaPhoto(selected_event[4], caption=text),
+                                         reply_markup=markup)
+
+async def read_more_callback(update, context):
+    """Редактирование сообщения, добавление подробной информации о выбранном событии."""
+    query = update.callback_query
+    print(query.data.split("_"))
+    await query.answer()
+    # Получение выбранного события
+    selected_event = [event for event in context.user_data["afisha_card"] if event[2][0] == query.data.split("_")[2]][0]
+    txt = split_text(selected_event[1])
+    # Создание кнопок
+    buy_button = InlineKeyboardButton(text="Купить билет", url=selected_event[5])
+    prev_button = InlineKeyboardButton(text="<", callback_data=f"prev_{selected_event[2][0]}")
+    next_button = InlineKeyboardButton(text=">", callback_data=f"next_{selected_event[2][0]}")
     keyboard = [[buy_button], [prev_button, next_button]]
     markup = InlineKeyboardMarkup(inline_keyboard=keyboard)
     # Редактирование сообщения
-    text = f"{selected_event[0]}\n\n{selected_event[2][1]} - {selected_event[3][1]}\n\n{selected_event[1]}"
-    await query.edit_message_text(text=text, reply_markup=markup)
-
+    text = f"{selected_event[0]}\n\n{selected_event[2][1]} - {selected_event[3][1]} - {selected_event[2][0]}\n\n{txt[1]}"
+    #await query.edit_message_text(text=text, reply_markup=markup)
+    await context.bot.edit_message_media(chat_id=query.message.chat_id, message_id=query.message.message_id,
+                                         media=InputMediaPhoto(selected_event[4], caption=text),
+                                         reply_markup=markup)
 async def prev_callback(update, context):
     """Переход к предыдущему событию в списке afisha_card."""
     query = update.callback_query
@@ -135,9 +192,12 @@ async def prev_callback(update, context):
         keyboard = [[buy_button, more_info_button], [prev_button, next_button]]
         markup = InlineKeyboardMarkup(inline_keyboard=keyboard)
         # Создание текста сообщения
-        text = f"{prev_event[0]}\n\n{prev_event[2][1]} - {prev_event[3][1]}"
+        text = f"{prev_event[0]}\n\n{prev_event[2][1]} - {prev_event[3][1]} - {prev_event[2][0]}"
         # Редактирование сообщения с новым событием и новыми кнопками
-        await query.message.edit_text(text=text, reply_markup=markup)
+        #await query.message.edit_text(text=text, reply_markup=markup)
+        await context.bot.edit_message_media(chat_id=query.message.chat_id, message_id=query.message.message_id,
+                                             media=InputMediaPhoto(prev_event[4], caption=text),
+                                             reply_markup=markup)
     else:
         # Если выбранное событие первое в списке, то просто отправляем сообщение без изменений
         await query.message.answer(text="Вы находитесь на первом событии.")
@@ -164,10 +224,13 @@ async def next_callback(update, context):
     keyboard = [[buy_button, more_info_button], [prev_button, next_button]]
     markup = InlineKeyboardMarkup(inline_keyboard=keyboard)
     # Создание текста сообщения
-    text = f"{next_event[0]}\n\n{next_event[2][1]} - {next_event[3][1]}"
+    text = f"{next_event[0]}\n\n{next_event[2][1]} - {next_event[3][1]} - {next_event[2][0]}"
     # Редактирование сообщения
-    await context.bot.edit_message_text(chat_id=query.message.chat_id, message_id=query.message.message_id,
-                                       text=text, reply_markup=markup)
+    #await context.bot.edit_message_text(chat_id=query.message.chat_id, message_id=query.message.message_id,
+     #                                  text=text, reply_markup=markup)
+    await context.bot.edit_message_media(chat_id=query.message.chat_id, message_id=query.message.message_id,
+                                         media=InputMediaPhoto(next_event[4], caption=text),
+                                         reply_markup=markup)
 
 
 async def giveaway(update, context):
@@ -202,6 +265,7 @@ def main():
 
     # Регистрация обработчиков
     application.add_handler(CallbackQueryHandler(callback=afisha_callback, pattern=r"event_\d+"))
+    application.add_handler(CallbackQueryHandler(callback=read_more_callback, pattern=r"read_more_\d+"))
     application.add_handler(CallbackQueryHandler(callback=more_info_callback, pattern=r"more_info_\d+"))
     application.add_handler(CallbackQueryHandler(callback=prev_callback, pattern=r"prev_\d+"))
     application.add_handler(CallbackQueryHandler(callback=next_callback, pattern=r"next_\d+"))
