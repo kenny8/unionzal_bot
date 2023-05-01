d694f24d23482f5f33922fc23ad2fe9a8c89adbc
kenny <fedoranisimov@gmail.com>
2023-05-01 05:00:33 +0300

    json_parser

---
+++ json_parser.py
@@ -0,0 +1,45 @@
+import sys  # импортируем модуль sys для использования функции exit()
+import requests  # модуль для выполнения HTTP-запросов
+import json  # модуль для работы с JSON-данными
+import re  # модуль для работы с регулярными выражениями
+from bs4 import BeautifulSoup  # библиотека для парсинга HTML-страниц
+from datetime import datetime, time #для работы с временим
+
+def json_afisha() -> None:
+    import time as tm
+    # выполнение HTTP-запроса и проверка на ошибки
+    try:
+        response = requests.get('https://unionzal.ru/rest/export/json/afisha')
+        response.raise_for_status()
+    except requests.exceptions.RequestException as error:
+        print(error)
+        sys.exit(1)  # выходим из программы в случае ошибки
+
+    data = json.loads(response.text)  # преобразуем полученные данные в формат JSON
+    afisha_card = []  # список для хранения полученной информации
+
+    # обработка данных из формата JSON и сохранение в списке afisha_card
+    for item in data:
+        afisha = []
+        title = item['title'][0]['value']  # получаем название мероприятия
+        soup = BeautifulSoup(item['body'][0]['value'], 'html.parser')  # получаем HTML-код описания мероприятия
+        txt = re.sub(r'\s+', ' ',
+                     soup.get_text()).strip()  # получаем текст из HTML-кода и Очищаем текст от управляющих символов
+        dt_start = datetime.fromisoformat(
+            item['field_data_koncerta'][0]['value'])  # получаем дату и время начала мероприятия
+        time_start = [dt_start.strftime('%d.%m.%y'), tm.strftime('%H:%M', tm.gmtime(
+            dt_start.timestamp()))]  # преобразование даты в строку в формате ['дд.мм.гг', 'чч:мм']
+        dt_end = datetime.fromisoformat(
+            item['field_data_koncerta_end'][0]['value'])  # получаем дату и время окончания мероприятия
+        time_end = [dt_end.strftime('%d.%m.%y'), tm.strftime('%H:%M', tm.gmtime(
+            dt_end.timestamp()))]  # преобразование даты в строку в формате ['дд.мм.гг', 'чч:мм']
+        banner = item['field_banner_dlya_glavnoy'][0]['url']  # получаем ссылку на баннер мероприятия
+        ticket = "https://quicktickets.ru/lipetsk-filarmoniya/" + item['field_qt_id'][0][
+            'value']  # получаем ссылку на покупку билетов мероприятия
+        afisha.extend([title, txt, time_start, time_end, banner, ticket])
+        afisha_card.append(afisha)  # добавляем полученную информацию в список afisha_card
+        print(afisha)  # выводим полученную информацию на экран
+
+    print("\n", afisha_card, "\n")  # выводим итоговый список на экран
+    print(data)  # выводим исходные данные в формате JSON на экран
+    return afisha_card
--- union_bot.py
+++ union_bot.py
@@ -1,66 +1,123 @@
 
 import logging
-from telegram import KeyboardButton, ReplyKeyboardMarkup
-from telegram import ForceReply, Update
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+from telegram import KeyboardButton, ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    CallbackQueryHandler,
+    MessageHandler,
+    filters,
+    CallbackContext,
+)
+from json_parser import json_afisha
 
 # Включение логгирования
 logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
 )
 logger = logging.getLogger(__name__)
 
-
+# Создание разметки клавиатуры для меню
 menu_keyboard = ReplyKeyboardMarkup(
     keyboard=[["Главная", "Афиша"], ["Исполнители", "Билеты"]]
 )
 
-
 # Определение обработчиков команд. Обычно они принимают два аргумента: update и context.
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def start(update, context):
     """Отправка сообщения, когда пользователь вводит команду /start."""
     user = update.effective_user
+    # Создание новой клавиатуры с кнопками выбора действия для пользователя
     reply_keyboard = ReplyKeyboardMarkup(
-        [["афиша", "розыгрыш билетов"], ["исполнители", "Обратная связь"]]
+        [["Афиша", "Розыгрыш билетов"], ["Исполнители", "Обратная связь"]]
     )
+    # Отправка сообщения пользователю
     await update.message.reply_html(
         rf"Привет, {user.mention_html()}!",
         reply_markup=reply_keyboard,
     )
 
 
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def help_command(update, context):
     """Отправка сообщения, когда пользователь вводит команду /help."""
+    # Отправка сообщения пользователю с инструкцией, что нужно делать
     await update.message.reply_text("Нужна помощь? помоги себе сам")
 
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def echo(update, context):
     """Эхо-ответ на сообщение пользователя."""
+    # Отправка сообщения пользователю, которое повторяет его входящее сообщение
     await update.message.reply_text(update.message.text)
 
-async def afisha(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Отправка сообщения, когда пользователь вводит 'афиша'."""
-    await update.message.reply_text("сегодня")
+
+async def afisha(update, context):
+    """Отправка сообщения, когда пользователь нажимает на кнопку 'Афиша'."""
+    afisha_card = json_afisha()#загрузка json
+    # Создание кнопки выбора даты концерта
+    keyboard_buttons = [
+        [InlineKeyboardButton(text=event[2][0], callback_data=event[2][0])]
+        for event in afisha_card
+    ]
+    # Создание клавиатуры с кнопками выбора даты концерта
+    keyboard = InlineKeyboardMarkup(inline_keyboard=keyboard_buttons)
+    # Отправка сообщения пользователю с клавиатурой выбора даты концерта
+    await context.bot.send_message(
+        chat_id=update.message.chat_id,
+        text="Выберите дату концерта",
+        reply_markup=keyboard,
+    )
+    # Сохранение данных афиши в пользовательскую базу данных бота
+    context.user_data["afisha_card"] = afisha_card
 
 
-def main() -> None:
+async def afisha_callback(update, context):
+    """Отправка сообщения с заголовком и текстом события, выбранного пользователем."""
+    query = update.callback_query
+    query.answer()
+    query.edit_message_text(text=f"Вы выбрали: {query.data}")
+    print("looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooool")
+
+
+async def giveaway(update, context):
+    """Отправка сообщения, когда пользователь нажимает на кнопку 'Розыгрыш билетов'."""
+    await update.message.reply_text("какой-то розыгрыш билетов")
+
+
+async def performers(update, context):
+    """Отправка сообщения, когда пользователь нажимает на кнопку 'Исполнители'."""
+    await update.message.reply_text("список исполнителей")
+
+
+async def feedback(update, context):
+    """Отправка сообщения, когда пользователь нажимает на кнопку 'Обратная связь'."""
+    await update.message.reply_text("напишите нам на почту")
+
+
+def main():
     """Запуск бота."""
     # Создание приложения и передача ему токена вашего бота.
     application = Application.builder().token("6241891253:AAEtTI5Ma8z34FM3fOusBJoLqI7xtRGLnTU").build()
-
     # Назначение обработчиков на различные команды в Telegram.
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
-    # Добавление обработчика сообщений для текста 'афиша'
-    application.add_handler(MessageHandler(filters=filters.Text('афиша'), callback=afisha))
-
-    # Обработка любых сообщений, кроме команд.
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
-
+    # Добавление обработчиков для каждой кнопки
+    application.add_handler(
+        MessageHandler(filters.Regex("^Афиша$"), callback=afisha)
+    )
+    application.add_handler(
+        CallbackQueryHandler(callback=afisha_callback)
+    )
+    application.add_handler(
+        MessageHandler(filters.Regex("^Розыгрыш билетов$"), callback=giveaway)
+    )
+    application.add_handler(
+        MessageHandler(filters.Regex("^Исполнители$"), callback=performers)
+    )
+    application.add_handler(
+        MessageHandler(filters.Regex("^Обратная связь$"), callback=feedback)
+    )
     # Запуск бота и ожидание его завершения пользователем (нажатие Ctrl-C).
     application.run_polling()
-
 
 if __name__ == "__main__":
     main()
