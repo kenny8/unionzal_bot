5fb7664eac5edba2f1c752389fecb2e3efd0c2a1
kenny <fedoranisimov@gmail.com>
2023-05-06 04:07:13 +0300

    giveaway done

---
+++ giveaway.py
@@ -0,0 +1,180 @@
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+import random
+import settings
+
+async def giveaway(update, context):
+    """Отправка сообщения, когда пользователь нажимает на кнопку 'Розыгрыш билетов'."""
+    admin_in = context.user_data.get("admin")
+    if admin_in is not None and context.user_data["admin"]:
+        if settings.START_GIVEAWAY[0] is False:
+            start_button = InlineKeyboardButton(text="начать", callback_data=f"giveaway_admin_start_0")
+            keyboard = InlineKeyboardMarkup(inline_keyboard=[[start_button]])
+        else:
+            stop_button = InlineKeyboardButton(text="завершить без победителя", callback_data=f"giveaway_admin_stop_0")
+            stop_win_button = InlineKeyboardButton(text="завершить с победителим",
+                                                   callback_data=f"giveaway_admin_stop_1")
+            check_button = InlineKeyboardButton(text="кол-во участников",
+                                                   callback_data=f"giveaway_admin_stop_2")
+            keyboard = InlineKeyboardMarkup(inline_keyboard=[[stop_button, stop_win_button], [check_button]])
+        await context.bot.send_message(chat_id=update.message.chat_id,
+                                       text="вы вошли в розыгрыши",
+                                       reply_markup=keyboard)
+    if admin_in is None or admin_in is not None and context.user_data["admin"] is False:
+        print("test")
+        if settings.START_GIVEAWAY[0]:
+            start_giveaway_button = InlineKeyboardButton(text="участвовать", callback_data=f"giveaway_user_0")
+            keyboard = InlineKeyboardMarkup(inline_keyboard=[[start_giveaway_button]])
+            await context.bot.send_message(chat_id=update.message.chat_id,
+                                           text=settings.START_GIVEAWAY[1],
+                                           reply_markup=keyboard)
+        else:
+            await context.bot.send_message(chat_id=update.message.chat_id,
+                                           text="пока нет розыгрышей")
+
+async def giveaway_callback(update, context):
+    query = update.callback_query
+    data = query.data.split("_")
+    print(data)
+    if data[1] == "admin" and data[2] == "start":
+        if data[3] == "0":
+            text = "введите текст розыгрыша"
+            start_giveaway_button = InlineKeyboardButton(text="назад", callback_data=f"giveaway_back_")
+            keyboard = InlineKeyboardMarkup(inline_keyboard=[[start_giveaway_button]])
+            await context.bot.edit_message_text(
+                chat_id=query.message.chat_id,
+                message_id=query.message.message_id,
+                text=text,
+                reply_markup=keyboard)
+            query = update.callback_query
+            context.user_data["giveaway_Text_start"] = True
+    elif data[1] == "admin" and data[2] == "stop":
+        if data[3] == "0":
+            settings.START_GIVEAWAY[0] = False
+            text = f"розыгрышь закончен по техническим причинам"
+            await context.bot.edit_message_text(
+                chat_id=query.message.chat_id,
+                message_id=query.message.message_id,
+                text=text
+            )
+            text_stop = "розыгрышь закончен по техническим причинам"
+            for participant in settings.START_GIVEAWAY[4]:
+                print(participant)
+                text = text_stop
+                await context.bot.send_message(chat_id=participant[1], text=text)
+        elif data[3] == "1":
+            print("answer")
+            print(settings.START_GIVEAWAY)
+            if len(settings.START_GIVEAWAY[4]) > 0:
+                winner = random.choice(settings.START_GIVEAWAY[4])
+                settings.START_GIVEAWAY[0] = False
+                win = "@" + str(winner[0])
+                text = f"розыгрышь закончен\n победитель : {win}"
+                await context.bot.edit_message_text(
+                    chat_id=query.message.chat_id,
+                    message_id=query.message.message_id,
+                    text=text
+                )
+                text_winner = f"Розыгрыш закончен. Вы победили, обращатся по поводу победы : @{settings.START_GIVEAWAY[3]}"
+                text_loser = "К сожалению, вы не выиграли в розыгрыше."
+                for participant in settings.START_GIVEAWAY[4]:
+                    print(participant)
+                    text = text_winner if participant == winner else text_loser
+                    await context.bot.send_message(chat_id=participant[1], text=text)
+            else:
+                settings.START_GIVEAWAY[0] = False
+                text = f"розыгрышь закончен\n участников не было"
+                await context.bot.edit_message_text(
+                    chat_id=query.message.chat_id,
+                    message_id=query.message.message_id,
+                    text=text
+                )
+        if data[3] == "2":
+            text = f"количество участников: {len(settings.START_GIVEAWAY[4])}"
+            start_giveaway_button = InlineKeyboardButton(text="назад", callback_data=f"giveaway_back_")
+            keyboard = InlineKeyboardMarkup(inline_keyboard=[[start_giveaway_button]])
+            await context.bot.edit_message_text(
+                chat_id=query.message.chat_id,
+                message_id=query.message.message_id,
+                text=text,
+                reply_markup=keyboard)
+    elif data[1] == "user":
+        if data[2] == "0":
+            user = update.effective_user
+            # Получаем ID пользователя и чата
+            user_id = update.effective_user.id
+            chat_id = update.effective_chat.id
+            user_status = await context.bot.get_chat_member(chat_id='@' + settings.CHAT, user_id=user_id)
+            if user_status.status == 'member' or user_status.status == 'creator' or user_status.status == 'administrator':
+                user = update.effective_user
+                username = user.username
+                if username not in [user_n[0] for user_n in settings.START_GIVEAWAY[4]]:
+                    await context.bot.edit_message_text(
+                        chat_id=chat_id,
+                        message_id=query.message.message_id,
+                        text='вы присоединились к розыгрышу'
+                    )
+                    settings.START_GIVEAWAY[4].append([user.username, chat_id])
+                else:
+                    await context.bot.edit_message_text(
+                        chat_id=chat_id,
+                        message_id=query.message.message_id,
+                        text='вы уже учавствуете'
+                    )
+                print(username)
+
+            else:
+                community_button = InlineKeyboardButton(text="Подписаться", url=f"t.me/{settings.CHAT}")
+                check_button = InlineKeyboardButton(text="Проверить", callback_data=f"giveaway_user_0_")
+                keyboard = InlineKeyboardMarkup(inline_keyboard=[[community_button, check_button]])
+                await context.bot.send_message(chat_id=chat_id,
+                                               text=f"Пожалуйста, подпишитесь на канал t.me/{settings.CHAT}, чтобы принять участие в розыгрыше.",
+                                               reply_markup=keyboard)
+    if data[1] == "back":
+        admin_in = context.user_data.get("admin")
+        if admin_in is not None and context.user_data["admin"]:
+            if settings.START_GIVEAWAY[0] is False:
+                start_button = InlineKeyboardButton(text="начать", callback_data=f"giveaway_admin_start_0")
+                keyboard = InlineKeyboardMarkup(inline_keyboard=[[start_button]])
+            else:
+                stop_button = InlineKeyboardButton(text="завершить без победителя",
+                                                   callback_data=f"giveaway_admin_stop_0")
+                stop_win_button = InlineKeyboardButton(text="завершить с победителим",
+                                                       callback_data=f"giveaway_admin_stop_1")
+                check_button = InlineKeyboardButton(text="кол-во участников",
+                                                    callback_data=f"giveaway_admin_stop_2")
+                keyboard = InlineKeyboardMarkup(inline_keyboard=[[stop_button, stop_win_button], [check_button]])
+            await context.bot.edit_message_text(
+                chat_id=query.message.chat_id,
+                message_id=query.message.message_id,
+                text="вы вошли в розыгрыши",
+                reply_markup=keyboard)
+        if admin_in is None or admin_in is not None and context.user_data["admin"] is False:
+            print("test")
+            if settings.START_GIVEAWAY[0]:
+                start_giveaway_button = InlineKeyboardButton(text="участвовать", callback_data=f"giveaway_user_0")
+                keyboard = InlineKeyboardMarkup(inline_keyboard=[[start_giveaway_button]])
+                await context.bot.edit_message_text(
+                    chat_id=query.message.chat_id,
+                    message_id=query.message.message_id,
+                    text=settings.START_GIVEAWAY[1],
+                    reply_markup=keyboard)
+            else:
+                await context.bot.edit_message_text(
+                    chat_id=query.message.chat_id,
+                    message_id=query.message.message_id,
+                    text="пока нет розыгрышей")
+
+async def giveaway_text(update, context):
+    search_query = context.user_data.get("giveaway_Text_start")
+    if search_query is not None:
+        if context.user_data["giveaway_Text_start"]:
+            user = update.effective_user
+            print(user.username)
+            # Получение введенного пользователем имени
+            text_admin = update.message.text
+            text = f"розыгрыш начат \n текст: {text_admin}"
+            await context.bot.send_message(chat_id=update.message.chat_id,
+                                           text=text)
+            context.user_data["giveaway_Text_start"] = False
+            settings.START_GIVEAWAY = [True, text_admin, 1, user.username, []]
--- union_bot.py
+++ union_bot.py
@@ -9,12 +9,10 @@ from telegram.ext import (
     filters,
     CallbackContext,
 )
-import random
-from telegram import InputMediaPhoto
-import asyncio
 import settings
 from afisha_ import afisha_callback, more_info_callback, read_more_callback, prev_callback, next_callback, afisha
 from performers import performers, performers_callback, performers_search_name
+from giveaway import giveaway, giveaway_callback, giveaway_text
 
 # Включение логгирования
 logging.basicConfig(
@@ -85,114 +83,6 @@ async def help_command(update, context):
     # Отправка сообщения пользователю с инструкцией, что нужно делать
     await update.message.reply_text("Нужна помощь? помоги себе сам")
 
-async def giveaway(update, context):
-    """Отправка сообщения, когда пользователь нажимает на кнопку 'Розыгрыш билетов'."""
-    admin_in = context.user_data.get("admin")
-    if admin_in is not None:
-        if context.user_data["admin"]:
-            if settings.START_GIVEAWAY[0] is False:
-                start_button = InlineKeyboardButton(text="начать", callback_data=f"giveaway_admin_start_0")
-                keyboard = InlineKeyboardMarkup(inline_keyboard=[[start_button]])
-            else:
-                stop_button = InlineKeyboardButton(text="завершить", callback_data=f"giveaway_admin_stop_")
-                keyboard = InlineKeyboardMarkup(inline_keyboard=[[stop_button]])
-            await context.bot.send_message(chat_id=update.message.chat_id,
-                                           text="вы вошли в розыгрыши",
-                                           reply_markup=keyboard)
-    elif admin_in is None or admin_in is not None and context.user_data["admin"] is False:
-        if settings.START_GIVEAWAY[0]:
-            start_giveaway_button = InlineKeyboardButton(text="учавствовать", callback_data=f"giveaway_user_0")
-            keyboard = InlineKeyboardMarkup(inline_keyboard=[[start_giveaway_button]])
-            await context.bot.send_message(chat_id=update.message.chat_id,
-                                           text=settings.START_GIVEAWAY[1],
-                                           reply_markup=keyboard)
-        else:
-            await context.bot.send_message(chat_id=update.message.chat_id,
-                                           text="пока нет розыгрышей")
-
-async def giveaway_callback(update, context):
-    query = update.callback_query
-    data = query.data.split("_")
-    print(data)
-    if data[1] == "admin" and data[2] == "start":
-        if data[3] == "0":
-            text = "Выберите длительность розыгрыша:"
-            reply_markup = InlineKeyboardMarkup([
-                [InlineKeyboardButton("1 день", callback_data=f"giveaway_admin_start_1_1")],
-                [InlineKeyboardButton("2 дня", callback_data=f"giveaway_admin_start_1_2")],
-                [InlineKeyboardButton("5 дней", callback_data=f"giveaway_admin_start_1_5")],
-                [InlineKeyboardButton("7 дней", callback_data=f"giveaway_admin_start_1_7")]
-            ])
-            await context.bot.send_message(chat_id=query.message.chat_id,
-                                           text=text,
-                                           reply_markup=reply_markup)
-        if data[3] == "1":
-            text = "введите текст розыгрыша"
-            reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("назад", callback_data=f"giveaway_admin_start_0_")]])
-            await context.bot.send_message(chat_id=query.message.chat_id,
-                                           text=text,
-                                           reply_markup=reply_markup)
-
-            query = update.callback_query
-            context.user_data["giveaway_Text_start"] = True
-            context.user_data["giveaway_time_start"] = int(data[4])
-    elif data[1] == "admin" and data[2] == "stop":
-        settings.START_GIVEAWAY[0] = False
-        text = "розыгрышь завершен"
-        await context.bot.send_message(chat_id=query.message.chat_id,
-                                       text=text)
-    elif data[1] == "user":
-        if data[2] == "0":
-            user = update.effective_user
-            # Получаем ID пользователя и чата
-            user_id = update.effective_user.id
-            chat_id = update.effective_chat.id
-            user_status = await context.bot.get_chat_member(chat_id='@' + settings.CHAT, user_id=user_id)
-            print(user_status.status)
-            if user_status.status == 'member' or user_status.status == 'creator' or user_status.status == 'administrator':
-                await context.bot.send_message(chat_id=chat_id,
-                                               text='вы присоединились к розыгрышу')
-                user = update.effective_user
-                print(user.username)
-                settings.START_GIVEAWAY[4].append(user.username)
-            else:
-                community_button = InlineKeyboardButton(text="Подписаться", url=f"t.me/{settings.CHAT}")
-                check_button = InlineKeyboardButton(text="Проверить", callback_data=f"giveaway_user_0_")
-                keyboard = InlineKeyboardMarkup(inline_keyboard=[[community_button, check_button]])
-                await context.bot.send_message(chat_id=chat_id,
-                                               text=f"Пожалуйста, подпишитесь на канал t.me/{settings.CHAT}, чтобы принять участие в розыгрыше.",
-                                               reply_markup=keyboard)
-
-async def giveaway_text(update, context):
-    search_query = context.user_data.get("giveaway_Text_start")
-    if search_query is not None:
-        if context.user_data["giveaway_Text_start"]:
-            user = update.effective_user
-            print(user.username)
-            # Получение введенного пользователем имени
-            text_admin = update.message.text
-            time = str(context.user_data["giveaway_time_start"])
-            text = f"розыгрыш начат \n текст: {text_admin} \n время: {time} дней"
-            await context.bot.send_message(chat_id=update.message.chat_id,
-                                           text=text)
-            context.user_data["giveaway_Text_start"] = False
-            settings.START_GIVEAWAY = [True, text_admin, int(time), user.username, []]
-
-async def choose_winner(context):
-    if settings.START_GIVEAWAY[0]:
-        if len(settings.START_GIVEAWAY[4]) > 0:
-            winner = random.choice(settings.START_GIVEAWAY[4])
-            return winner
-    return None
-
-async def end_giveaway(context):
-    winner = await choose_winner(context)
-    print("lollllllllllllll")
-    if winner is not None:
-        await context.bot.send_message(chat_id='@' + settings.CHAT, text=f'Победитель розыгрыша: {winner}!')
-    else:
-        await context.bot.send_message(chat_id='@' + settings.CHAT, text='Розыгрыш окончен, но нет победителя :(')
-
 async def feedback(update, context):
     """Отправка сообщения, когда пользователь нажимает на кнопку 'Обратная связь'."""
     await update.message.reply_text("напишите нам на почту")
@@ -223,6 +113,7 @@ def main():
     application.add_handler(CallbackQueryHandler(callback=giveaway_callback, pattern=r"giveaway_admin_start_\d*"))
     application.add_handler(CallbackQueryHandler(callback=giveaway_callback, pattern=r"giveaway_admin_stop_\d*"))
     application.add_handler(CallbackQueryHandler(callback=giveaway_callback, pattern=r"giveaway_user_\d*"))
+    application.add_handler(CallbackQueryHandler(callback=giveaway_callback, pattern=r"giveaway_back_\d*"))
     application.add_handler(
         MessageHandler(filters.Regex(f"^{settings.PASSWORD}$"), callback=admin)
     )
@@ -243,9 +134,6 @@ def main():
     )
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, giveaway_text))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, performers_search_name))
-    # запустить функцию через 5 минут после начала розыгрыша
-    loop = asyncio.get_event_loop()
-    loop.call_later(300, asyncio.ensure_future, end_giveaway(context))
     # Запуск бота и ожидание его завершения пользователем (нажатие Ctrl-C).
     application.run_polling()
 
