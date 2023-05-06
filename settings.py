4aea3f280c60a5b7050db036048e3332fd434f99
kenny <fedoranisimov@gmail.com>
2023-05-06 17:17:59 +0300

    fixed bitton

--- settings.py
+++ settings.py
@@ -8,10 +8,15 @@ JSON_AFISHA_URL = "https://unionzal.ru/rest/export/json/afisha"
 JSON_PERSONS_URL = "https://unionzal.ru/rest/export/json/persons"
 TIPE = {2: "Солисты", 20: "Коллективы", 4: "Ведущие концертов", 51: "Концертмейстеры"}
 PASSWORD = "o76cy5zl2y"
+
 START_GIVEAWAY = [True, "пример текста розыгрыша", 1, "Geralt_f_r_o_m_Rivia", []]
+
 FEEDBACK_USER = [["@Geralt_f_r_o_m_Rivia", "1 сообщение"], ["@Geralt_f_r_o_m_Rivia", "2 сообщение"], ["@Geralt_f_r_o_m_Rivia", "3 сообщение"]]
+
 USERS = []
+
 CHAT = "test12345671235"
+
 now = datetime.now()
 AFISHA_CARD = json_afisha()
 PERSONS_CARD = json_persons()
