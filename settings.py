5295022f0c203ab63edf1385a5544c4fb7e8e3c3
kenny <fedoranisimov@gmail.com>
2023-05-06 17:27:15 +0300

    bit

--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
 from json_parser import json_afisha, json_persons
 from datetime import datetime
+import pickle
 
 MAX_MESSAGE_LENGTH = 900
 MAIN_WALLPAPERS = "https://unionzal.ru/sites/default/files/2020-11/presentation-wrapper.jpg"
@@ -13,7 +14,15 @@ START_GIVEAWAY = [True, "пример текста розыгрыша", 1, "Gera
 
 FEEDBACK_USER = [["@Geralt_f_r_o_m_Rivia", "1 сообщение"], ["@Geralt_f_r_o_m_Rivia", "2 сообщение"], ["@Geralt_f_r_o_m_Rivia", "3 сообщение"]]
 
+with open('feedback_fl.txt', 'wb') as file:
+    pickle.dump(FEEDBACK_USER, file)
+
 USERS = []
+
+with open('feedback_fl.txt', 'rb') as file:
+    FEEDBACK_USER = pickle.load(file)
+
+print(FEEDBACK_USER)
 
 CHAT = "test12345671235"
 
